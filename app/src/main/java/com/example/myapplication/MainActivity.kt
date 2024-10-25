e3d4efb1d66b63f1f88e89b3756fde567a1c6b38
Artur Leonel <Arturleonel08@outlook.com>
2024-10-25 10:50:06 -0300

    testando

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity2::class.java))
+        }, 2000)
     }
 }
